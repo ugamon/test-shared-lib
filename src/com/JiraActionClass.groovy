e66b33b9263933aa06cb560cde57c33e79e2fd3f
Yuriy Bulich <>
2022-02-04 01:40:33 +0300

    Changed naming for the class

--- src/com/JiraActionClass.groovy
+++ src/com/JiraActionClass.groovy
@@ -1,9 +1,10 @@
 #!/usr/bin/env groovy
+
 package com
 //import org.jenkinsci.plugins.workflow.steps.Step
 import org.thoughtslive.jenkins.plugins.jira.steps.AddCommentStep
 
-class JiraAction {
+class JiraActionClass {
     void add_comment(String comment, String id) {
         new AddCommentStep(id, comment)
     }
