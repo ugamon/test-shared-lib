64fd03b8db15a24135c127b87e6b80768c18a70a
Yuriy Bulich <>
2022-02-04 09:52:46 +0300

    Added add_comment method to JiraActionClass.groovy

--- src/com/JiraActionClass.groovy
+++ src/com/JiraActionClass.groovy
@@ -2,11 +2,12 @@
 
 package com
 //import org.jenkinsci.plugins.workflow.steps.Step
-//import org.thoughtslive.jenkins.plugins.jira.steps.AddCommentStep
+import org.thoughtslive.jenkins.plugins.jira.steps.AddCommentStep
 //import org.thoughtslive.jenkins.plugins.jira.steps.TransitionIssueStep
 
 class JiraActionClass {
-    void add_comment(String comment, String id) {
-        new TransitionIssueStep('id', [transition: [id: 1]])
+    static def add_comment(String comment, String id) {
+        def comment_object = new AddCommentStep(comment, id)
+        return comment_object
     }
 }
