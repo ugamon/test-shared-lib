bb18b54f6a356f4edc2cea2478c433b2a08a1e9f
Yuriy Bulich <>
2022-02-04 10:17:08 +0300

    Major changes

--- src/com/JiraActionClass.groovy
+++ src/com/JiraActionClass.groovy
@@ -1,12 +1,7 @@
 #!/usr/bin/env groovy
 
 package com
-
-import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
-
-//import org.jenkinsci.plugins.workflow.steps.Step
 import org.thoughtslive.jenkins.plugins.jira.steps.AddCommentStep
-//import org.thoughtslive.jenkins.plugins.jira.steps.TransitionIssueStep
 
 class JiraActionClass {
     static def add_comment(String comment, String id) {
